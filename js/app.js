55f16be9b282a73091b2b349043027641ddc22c1
sharvil <sharvil@tamu.edu>
2017-03-17 22:57:32 -0500

    Added day wise schedule

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
 var app = angular.module("myApp",[]);
 
 app.controller('jsonFetcher',function($scope, $http){
-
   $http.get('json/gadetails.json').success(function(data){
       $scope.assistants = data;
   });
-
+});
+app.controller('ScheduleFetcher',function($scope, $http){
+  var day = new Date();
+  var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  var json_name = 'json/schedule'+days[day.getDay()]+'.json'
+  $http.get(json_name).success(function(data){
+      $scope.schedule = data;
+  });
 });
